3416aa13d840e406edd2a1e949c859733baa1181
Julio Vieira <juliocesarjuniorjja@hotmail.com>
2023-10-03 19:20:47 -0300

    add import React from react no App,jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React from "react"
 import Images from "Images.jsx"
 
 function App() {
--- src/Images.jsx
+++ src/Images.jsx
@@ -1,9 +1,9 @@
-import React from "react"
-import githubIcon from "assets/github-icon.png"
-import instagramIcon from "assets/instagram-icon.png"
-import linkedinIcon from "assets/linkedin-icon.png"
-import mailIcon from "assets/mail-icon.png"
-import profilePic from "assets/profile-pic.png"
+import React from "react";
+import githubIcon from "./assets/github-icon.png";
+import instagramIcon from "./assets/instagram-icon.png";
+import linkedinIcon from "./assets/linkedin-icon.png";
+import mailIcon from "./assets/mail-icon.png";
+import profilePic from "./assets/profile-pic.png";
 
 const Images = {
     githubIcon,
@@ -11,6 +11,6 @@ const Images = {
     linkedinIcon,
     mailIcon,
     profilePic,
-}
+};
 
 export default Images;
