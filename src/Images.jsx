f266c4bce1f17c83a60078b0475c2dce8e9b2ea0
Julio Vieira <juliocesarjuniorjja@hotmail.com>
2023-10-03 17:23:46 -0300

    first commit

---
+++ src/Images.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import githubIcon from "./assets/Github-Icon.png"
+import instagramIcon from "./assets/Instagram-Icon.png"
+import linkedinIcon from "./assets/Linkedin-Icon.png"
+import mailIcon from "./assets/Mail-Icon.png"
+import profilePic from "./assets/profile-pic.png"
+
+const Images = {
+    githubIcon,
+    instagramIcon,
+    linkedinIcon,
+    mailIcon,
+    profilePic,
+}
+
+export default Images
